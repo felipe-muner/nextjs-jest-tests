26d8551778a7993dacab6d21327f5881773d9dd8
felipemuner <felipe.muner@gmail.com>
2021-06-22 14:27:30 +0700

    fixed error of importing css in jest

--- tests/test.test.js
+++ tests/test.test.js
@@ -1,12 +1,21 @@
 import Text from '../components/Text';
+import Index from '../components/Index';
 import { shallow } from "enzyme";
 
-test('check counter', ()=> {
+test('Div Amount', ()=> {
   const render = shallow(<Text />);
   const divs = render.find('div')
-  const buttons = render.find('button')
-  console.log(buttons)
-  // const button = getByText("Increase");
   expect(divs).toHaveLength(1)
-  expect(buttons).toHaveLength(1)
+});
+
+test('Button Amount', ()=> {
+  const render = shallow(<Text />);
+  const buttons = render.find('button')
+  expect(buttons).toHaveLength(2)
+});
+
+test('H1 Amount', ()=> {
+  const render = shallow(<Index />);
+  const h1s = render.find('h1')
+  expect(h1s).toHaveLength(1)
 });
